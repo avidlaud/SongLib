c4e964c66d067c3a3366c822205655a857647ae4
avidlaud <avidlaud@gmail.com>
2020-02-12 15:42:33 -0500

    Implement FXML variables

--- SongLib/src/view/Controller.java
+++ SongLib/src/view/Controller.java
@@ -1,5 +1,33 @@
 package view;
 
+import javafx.fxml.FXML;
+import javafx.stage.Stage;
+import javafx.scene.control.*;
+
+
 public class Controller {
 
+	Stage mainStage;
+	
+	@FXML
+	private Label labelName;
+	
+	@FXML
+	private Label labelArtist;
+	
+	@FXML
+	private Label labelAlbum;
+	
+	@FXML
+	private Label labelYear;
+	
+	@FXML
+	private Button buttonAdd;
+	
+	@FXML
+	private Button buttonEdit;
+	
+	@FXML
+	private Button buttonDelete;
+	
 }
