cb538bfceba4bd8bd6be0aba16d5b4e746361b78
Ken Erickson <ken_c@172.31.145.194>
2020-02-12 10:52:59 -0500

    Created App.java and added a necessary method to Controller.java

--- SongLib/src/view/Controller.java
+++ SongLib/src/view/Controller.java
@@ -1,5 +1,11 @@
 package view;
 
-public class Controller {
+import javafx.stage.Stage;
 
+public class Controller {
+	Stage mainStage;
+	
+	public void setMainStage(Stage stage) {
+		mainStage = stage;
+	}
 }
